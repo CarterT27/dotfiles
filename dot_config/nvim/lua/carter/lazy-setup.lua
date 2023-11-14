40cc7adaa4122eed8446eca9b9d63605abef1d00
Carter Tran <carter.tran@gmail.com>
2023-11-13 20:12:53 -0800

    Added lazygit.nvim to nvim config

--- dot_config/nvim/lua/carter/lazy-setup.lua
+++ dot_config/nvim/lua/carter/lazy-setup.lua
@@ -80,4 +80,11 @@ require("lazy").setup({
     },
     { 'dccsillag/magma-nvim', run = ':UpdateRemotePlugins' },
     -- { "lervag/vimtex", lazy = false },
+    {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
 })
