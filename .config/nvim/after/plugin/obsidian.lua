8287de945f651d80fc15ddc606dde5877405006b
Carter Tran <carter.tran@gmail.com>
2024-04-08 10:42:12 -0700

    enabled link opening in obsidian

--- .config/nvim/after/plugin/obsidian.lua
+++ .config/nvim/after/plugin/obsidian.lua
@@ -18,6 +18,14 @@ require("obsidian").setup({
     templates = {
         subdir = "templates",
     },
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+    -- URL it will be ignored but you can customize this behavior here.
+    ---@param url string
+    follow_url_func = function(url)
+        -- Open the URL in the default web browser.
+        vim.fn.jobstart({"open", url})  -- Mac OS
+        -- vim.fn.jobstart({"xdg-open", url})  -- linux
+    end,
 })
 
 vim.keymap.set("n", "<C-n>d", vim.cmd.ObsidianToday)
