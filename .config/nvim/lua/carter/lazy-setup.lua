6512a73352562247d9511394be532812899a3f84
Carter Tran <carter.tran@gmail.com>
2024-06-25 23:24:24 -0700

    added typst preview

--- .config/nvim/lua/carter/lazy-setup.lua
+++ .config/nvim/lua/carter/lazy-setup.lua
@@ -97,6 +97,12 @@ require("lazy").setup({
             }
         }
     },
+    {
+        'chomosuke/typst-preview.nvim',
+        lazy = false, -- or ft = 'typst'
+        version = '0.3.*',
+        build = function() require 'typst-preview'.update() end,
+    }
     -- "Exafunction/codeium.vim",
     -- "github/copilot.vim"
 })
