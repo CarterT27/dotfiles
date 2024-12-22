baed551f213e00aa6526a086f03332c3353aff08
Carter Tran <carter.tran@gmail.com>
2024-12-22 00:24:24 -0600

    change wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Batman"
+config.color_scheme = "Catppuccin Macchiato"
 
 config.font = wezterm.font("Fira Code")
 config.font_size = 14
